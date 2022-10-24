33574db2640ccdc4a465525e1b0c609a645859aa
johnbriancapate <johnbriancapate@gmail.com>
2022-10-25 05:06:49 +0800

    add nodejs modules for the date

--- app.js
+++ app.js
@@ -1,61 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const date = require(__dirname + "/date.js")
+
+app.use(bodyParser.urlencoded({extended: true,}));
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
-let items = ['Cook', 'Eat'];
+app.set("view engine", "ejs");
 
 
-
+let items = ["Cook", "Eat"];
+let workItem = [];
 
 app.get("/", (req, res) => {
-  let today = new Date();
-  const currentDay = today.getDay();
-  let day = '';
-  const name = 'John Brian';
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday';
-      break;
-    case 1:
-      day = 'Monday';
-      break;
-    case 2:
-      day = 'Tuesday';
-      break;
-    case 3:
-      day = 'Wednesday';
-      break;
-    case 4:
-      day = 'Thursday';
-      break;
-    case 5:
-      day = 'Friday';
-      break;
-    case 6:
-      day = 'Saturday';
-      break;
-    default:
+  const day = date.getDate();
+  res.render("list", {listTitle: day,newItem: items,});
+});
 
+app.post("/", (req, res) => {
+  if(req.body.button == "Work"){
+    workItem.push(req.body.newItem);
+    res.redirect("/work");
+  }else{
+    items.push(req.body.newItem);
+    res.redirect("/");
   }
-
-  res.render('list', {
-    kindOfDay: day,
-    name: name,
-    newItem: items,
-
-  });
-
-
 });
 
-app.post('/', (req, res) => {
-  items.push(req.body.newItem);
-  res.redirect('/');
+app.get("/work", (req,res) => {
+  res.render('list', {listTitle: 'Work List', newItem: workItem});
 });
+
+app.get("/about", (req, res) => {
+  res.render('about');
+})
 
 app.listen(3000, () => {
   console.log("server is running...");

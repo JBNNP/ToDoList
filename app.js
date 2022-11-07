c3bbc8c6a8bdadaf3eb936e8543ae55c35bc0a2a
johnbrian <johnbriancapate@gmail.com>
2022-11-08 00:58:41 +0800

    add Procfile, giignore,  engine version

--- app.js
+++ app.js
@@ -148,7 +148,12 @@ app.post("/delete", (req, res)=> {
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`server is running...`);
 });
